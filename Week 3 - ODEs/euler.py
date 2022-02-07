b3db3fe238a8858f495096a985e9ea5fdd2a7fd1
crispianm <45373428+crispianm@users.noreply.github.com>
2022-02-07 15:57:32 +0000

    Update euler.py with solve_ode

--- Week 3 - ODEs/euler.py
+++ Week 3 - ODEs/euler.py
@@ -1,29 +1,57 @@
+#!/usr/bin/env python3
+#
+#  ode_solve.py
+#
+#  Usage:
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def main(method,output):
+
+
 ## Functions
+# dx/dt = f(x,t)
+def f(x,t):
+    return x
 
-def euler_step(f, t, deltat_max, s):
-    for i in range(0, len(t) - 1):
-        s[i + 1] = s[i] + deltat_max*f(t[i], s[i])
+# Euler step
+# f - the function
+# tn - the time to estimate at
+# h - the timestep
+# x0 - the initial condition
+def euler_step(f, tn, h, x0):
+    xn1 = x0 + h*f(x0, tn)
+    return xn1
 
-def solve_to():
 
-def solve_ode():
-    print('hi')
+# solve to
+def solve_to(f,x0,t1,t2,deltat_max):
+    timesteps = int((t2-t1)/deltat_max)
+    x = x0
+    for step in range(timesteps):
+        x = euler_step(f, step, deltat_max, x)
+    return x
 
 
+# solve the ode
+def solve_ode(f,t,x0,deltat_max):
 
-plt.style.use('seaborn-poster')
-%matplotlib inline
+    x_series = [x0]
+    x = x0
+    for i in range(len(t)-1):
+        
+        x = solve_to(f,x,t[i],t[i+1],deltat_max)
+        x_series.append(x)
 
-# Define parameters
-f = lambda t, s: np.exp(-t) # ODE
-deltat_max = 0.1 # Step size
-t = np.arange(0, 1 + deltat_max, deltat_max) # Numerical grid
-s0 = -1 # Initial Condition
+    return x_series
 
-# Explicit Euler Method
-s = np.zeros(len(t))
-s[0] = s0
+# if __name__ == "__main__":
+#     import sys
+#     main(*sys.argv[1:])
 
+# print(solve_to(f, 1, 0, 1, 0.0001))
+# print(solve_to(f, 2.718, 1, 2, 0.0001))
+# print(solve_to(f, 1, 2, 3, 0.0001))
+print(solve_ode(f, [0,.1,.2,.3], 1, 0.0001))
