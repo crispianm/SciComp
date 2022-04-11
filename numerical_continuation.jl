3d43c48c5ec1418ae48d1c7b2e5d94cdcf01ec05
Crispian Morris <crispianm2000@gmail.com>
2022-04-11 21:59:01 +0100

    Create continuation files

---
+++ numerical_continuation.jl
@@ -0,0 +1,12 @@
+function pseudo_arclength(f, u0_, alpha_range, delta_alpha, discretisation)
+
+    
+
+end
+
+
+function natural_parameter_continuation(f, u0_, alpha_range, delta_alpha, discretisation)
+    
+    
+
+end
