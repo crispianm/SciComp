acc0cbf6c3b7f62bf1066a4fa1441e4320f5e7ab
Crispian Morris <crispianm2000@gmail.com>
2022-04-12 11:30:51 +0100

    Fixed optional arguments bug

--- tests/continuation_test.jl
+++ tests/continuation_test.jl
@@ -1,4 +1,4 @@
-function hopf2d(u, t, beta, sigma=-1.0)
+function algebraic(x, c, arg...)
 
     u1, u2 = u
     du1dt = beta*u1 - u2 + sigma*u1*(u1^2 + u2^2)
@@ -8,7 +8,7 @@ function hopf2d(u, t, beta, sigma=-1.0)
 end
 
 
-function hopf2d(u, t, beta, sigma=-1.0)
+function hopf2d(u, t, beta, sigma=-1.0, arg...)
 
     u1, u2 = u
     du1dt = beta*u1 - u2 + sigma*u1*(u1^2 + u2^2)
@@ -17,7 +17,7 @@ function hopf2d(u, t, beta, sigma=-1.0)
     return [du1dt du2dt]
 end
 
-function hopf2d_modified(u, t, beta, sigma=-1.0)
+function hopf2d_modified(u, t, beta, sigma=-1.0, arg...)
 
     u1, u2 = u
     du1dt = beta*u1 - u2 - sigma*u1*(u1^2 + u2^2) + sigma*u1*(u1^2 + u2^2)^2
