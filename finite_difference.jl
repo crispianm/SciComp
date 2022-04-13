30eb32febcb8eb6980d89b88c800377821ddd997
Crispian Morris <crispianm2000@gmail.com>
2022-04-13 14:10:14 +0100

    Added forward euler function to finite diff

--- finite_difference.jl
+++ finite_difference.jl
@@ -1,5 +1,8 @@
-function forward_euler(init_condition, kappa, L, T, mx, mt)
-    
+using PlotlyJS
+using LinearAlgebra
+
+function forward_euler(u_I, kappa, L, T, mx, mt)
+
     # Set up the numerical environment variables
     x = LinRange(0, L, mx+1)     # mesh points in space
     t = LinRange(0, T, mt+1)     # mesh points in time
@@ -9,6 +12,9 @@ function forward_euler(init_condition, kappa, L, T, mx, mt)
     println("deltax = ", deltax)
     println("deltat = ", deltat)
     println("lambda = ", lmbda)
+
+    # Create A_FE matrix
+    A_FE = Tridiagonal(ones(mx-1)*(lmbda), ones(mx)*(1 - 2*lmbda), ones(mx-1)*(lmbda))
 
     # Set up the solution variables
     u_j = zeros(size(x))        # u at current time step
@@ -23,16 +29,56 @@ function forward_euler(init_condition, kappa, L, T, mx, mt)
     for j in 1:mt
         # Forward Euler timestep at inner mesh points
         # PDE discretised at position x[i], time t[j]
-        for i in 2:mx
-            u_jp1[i] = u_j[i] + lmbda*(u_j[i-1] - 2*u_j[i] + u_j[i+1])
-        end
+
+        u_jp1[2:end] = A_FE*(u_j[2:end])
+
         # Boundary conditions
-        u_jp1[1] = 0; u_jp1[mx+1] = 0
+        u_jp1[1] = 0
+        u_jp1[mx+1] = 0
             
         # Save u_j at time t[j+1]
         u_j[:] = u_jp1[:]
     end
 
-
+    # println("x: ", x)
+    # println("u_j: ", u_j) 
     return x, u_j
 end
+
+
+kappa = 1   # diffusion constant
+L = 1.0         # length of spatial domain
+T = 0.5         # total time to solve for
+
+function u_I(x)
+    # initial temperature distribution
+    y = sin.(pi*x/L)
+    return y
+end
+
+function u_exact(x,t)
+    # the exact solution
+    y = exp.(-kappa*(pi^2/L^2)*t)*sin.(pi*x/L)
+    return y
+end
+
+# Set numerical parameters
+mx = 10     # number of gridpoints in space
+mt = 1000   # number of gridpoints in time
+
+x, u_j = forward_euler(u_I, kappa, L, T, mx, mt);
+
+# Plot the final result and exact solution
+
+xx = LinRange(0,L,250)
+
+# Create traces
+exact = scatter(x=xx, y=u_exact(xx,T), mode="lines", name="exact", showlegend=true)
+num = scatter(x=x, y=u_j, mode="markers", name="num", showlegend=true)
+
+layout = Layout(
+    xaxis_title = "x",
+    yaxis_title = "u(x,0.5)"
+    )
+
+plot([exact, num], layout)
